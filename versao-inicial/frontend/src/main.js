d609c5e7170437521825dffcfd5c013d6e8696eb
Arthur <arthurclementte@hotmail.com>
2020-09-05 14:38:25 -0300

    Adicionado temporariamente o token que dá acesso as estatísticas do backend.

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORÁRIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFydGh1ciBKb3NlIENsZW1lbnRlIiwiZW1haWwiOiJhcnRodXJjbGVtZW50dGVAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTk5MzI2MjU2LCJleHAiOjE1OTk1ODU0NTZ9.K6vX6ximFC6PemsCe5lOwu0AL-XB2JgBBzJftigundQ'
+
 new Vue({
   store,
   router,
