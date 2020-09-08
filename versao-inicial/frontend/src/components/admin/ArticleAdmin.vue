06b2672275e62a4359f6deeec22f7a46c293392f
Arthur <arthurclementte@hotmail.com>
2020-09-08 20:11:56 -0300

    Implementada a paginação dos artigos.

--- versao-inicial/frontend/src/components/admin/ArticleAdmin.vue
+++ versao-inicial/frontend/src/components/admin/ArticleAdmin.vue
@@ -53,6 +53,7 @@
                 </b-button>
             </template>
         </b-table>
+        <b-pagination size="md" v-model="page" :total-rows="count" :per-page="limit" />
     </div>
 </template>
 
@@ -84,7 +85,7 @@ export default {
     },
     methods: {
         loadArticles() {
-            const url = `${baseApiUrl}/articles`
+            const url = `${baseApiUrl}/articles?page=${this.page}`
             axios.get(url).then(res => {
                 this.articles = res.data.data
                 this.count = res.data.count
@@ -135,6 +136,11 @@ export default {
                     return { value: user.id, text: `${user.name} - ${user.email}` }
                 })
             })
+        }
+    },
+    watch: {
+        page() {
+            this.loadArticles()
         }
     },
     mounted() {
