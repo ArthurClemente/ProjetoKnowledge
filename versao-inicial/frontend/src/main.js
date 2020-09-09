abd4b6cb90393ed523e4841ccec6cd29bf564bc2
Arthur <arthurclementte@hotmail.com>
2020-09-09 15:04:41 -0300

    Excluído o token de login temporário.

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -10,9 +10,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// TEMPORÁRIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFydGh1ciBKb3NlIENsZW1lbnRlIiwiZW1haWwiOiJhcnRodXJjbGVtZW50dGVAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTk5NTg3Njg0LCJleHAiOjE2MDAxOTI0ODR9.QDBKnpRfaaXiNtJkD6LoyKszvq5RmpPElvNkhqxGO98'
-
 new Vue({
   store,
   router,
